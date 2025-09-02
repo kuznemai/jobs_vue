6f33e582ecbca236a43419ca76e2da76c856a873
Kuznetsov Dmitry <indycar111@gmail.com>
2025-09-02 15:12:01 +0300

    github pages had been added

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jobs_vue/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -16,3 +17,6 @@ export default defineConfig({
     },
   },
 })
+
+
+
